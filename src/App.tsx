34577f1f1220b062809d0bfa1e94838f8bfd26cb
Deo Cherisme <deolenscherisme1000@gmail.com>
2026-02-08 02:27:43 -0500

    Add loading state with CircularProgress

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import{ useState} from "react"
 import SearchBar from "./components/SearchBar"
 import type { WeatherData } from "./types/weather";
 import WeatherCard from "./components/WeatherCard";
-
+import { CircularProgress } from "@mui/material";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
 
   const[weather , setWeather] = useState<WeatherData | null>(null);
-
+  const[loading, setLoading] = useState(false);
   function handleSearch(city: string){
+    setLoading(true);
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
   .then((response) => response.json())
-  .then((data) => setWeather(data))
+  .then((data) => {
+    setWeather(data);
+    setLoading(false);
+  })
   }
 
 
@@ -21,7 +25,7 @@ function App() {
     <div className="App">
       <h1>Weather Dashboard</h1>
       <SearchBar onSearch={handleSearch} />
-      {weather && <WeatherCard weather={weather} />}
+      {loading ? <CircularProgress /> : weather && <WeatherCard weather={weather} />}
     </div>
   )
 }
