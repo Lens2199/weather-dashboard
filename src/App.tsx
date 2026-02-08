ccf3493290b7a16a4ea5689288dcb0ee5ff79462
Deo Cherisme <deolenscherisme1000@gmail.com>
2026-02-08 02:45:42 -0500

    Add error handling with Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,39 @@ import{ useState} from "react"
 import SearchBar from "./components/SearchBar"
 import type { WeatherData } from "./types/weather";
 import WeatherCard from "./components/WeatherCard";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress , Alert } from "@mui/material";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
 
   const[weather , setWeather] = useState<WeatherData | null>(null);
   const[loading, setLoading] = useState(false);
-  function handleSearch(city: string){
-    setLoading(true);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
-  .then((response) => response.json())
-  .then((data) => {
-    setWeather(data);
-    setLoading(false);
-  })
-  }
+  const[error, setError] = useState<string | null>(null);
+function handleSearch(city: string){
+  setLoading(true);
+  setError(null);
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setWeather(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError(error.message);
+      setLoading(false);
+    });
+}
 
-
-
-  return (
+return (
     <div className="App">
       <h1>Weather Dashboard</h1>
       <SearchBar onSearch={handleSearch} />
+      {error && <Alert severity="error">{error}</Alert>}
       {loading ? <CircularProgress /> : weather && <WeatherCard weather={weather} />}
     </div>
   )
